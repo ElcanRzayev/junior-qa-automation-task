6f807bea2403573e872b5d990d78619142b92c43
Elcan Rzayev <elcanrza1905@gmail.com>
2026-02-14 01:21:13 +0400

    Create API config

---
+++ src/test/java/com/elcan/jokes/config/ApiConfig.java
@@ -0,0 +1,8 @@
+package com.elcan.jokes.config;
+
+public final class ApiConfig {
+    private ApiConfig() {}
+
+    // Official Joke API base URL
+    public static final String BASE_URL = "https://official-joke-api.appspot.com";
+}
